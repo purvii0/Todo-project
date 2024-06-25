d005d419740f1e3ceb85b3690acdad0a32499c18
Purvi Gupta <singhalpurvi05@gmail.com>
2024-06-25 14:35:35 +0530

    added css

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,22 +1,30 @@
+import React from "react";
+import "./TodoItem.css";
+
 function TodoItem({ item, handleToggle, handleDelete }) {
   return (
-    <div>
-      {item.title} - {item.status ? "Done" : "Not Done"}
-      <button
-        onClick={() => {
-          handleToggle(item.id);
-        }}
-      >
-        {" "}
-        Toggle
-      </button>
-      <button
-        onClick={() => {
-          handleDelete(item.id);
-        }}
-      >
-        Delete
-      </button>
+    <div className="todo-item">
+      <div className="todo-item-title">{item.title}</div>
+      <div className={`todo-item-status ${item.status ? "done" : "not-done"}`}>
+        {item.status ? "Done" : "Not Done"}
+      </div>
+      <div className="todo-item-buttons">
+        <button
+          onClick={() => {
+            handleToggle(item.id);
+          }}
+        >
+          Toggle
+        </button>
+        <button
+          className="delete-btn"
+          onClick={() => {
+            handleDelete(item.id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
